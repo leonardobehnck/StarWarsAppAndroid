52017bb32a7419e97ab6dd14698ebf73cd421ca4
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-08-13 18:44:17 -0300

    cleaning code

--- app/src/main/java/com/example/starwarsapp/domain/Character.kt
+++ app/src/main/java/com/example/starwarsapp/domain/Character.kt
@@ -11,17 +11,3 @@ data class Character(
   val gender: String,
   val homeWorld: String,
   )
-
-
-
-
-//	"name": "Luke Skywalker",
-//	"height": "172",
-//	"mass": "77",
-//	"hair_color": "blond",
-//	"skin_color": "fair",
-//	"eye_color": "blue",
-//	"birth_year": "19BBY",
-//	"gender": "male",
-//	"homeworld": "https://swapi.dev/api/planets/1/",
-//
